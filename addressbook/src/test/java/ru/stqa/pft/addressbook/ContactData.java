b56677e24e98e80be6220316c369d8d1e31724db
Мамедов Тимур <Tima_mamedov_1991@mail.ru>
2020-08-28 14:40:26 +0300

    Тест для создания контактов в адресной книге
    Домашнее задание №4

---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String middleName;
+  private final String mail;
+  private final String number;
+
+  public ContactData(String firstName, String middleName, String mail, String number) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.mail = mail;
+    this.number = number;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+}
