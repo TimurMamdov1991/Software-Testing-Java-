01a693006ebf37d09c08584fcebe4b2d89e1d5eb
Мамедов Тимур <Tima_mamedov_1991@mail.ru>
2020-10-17 11:10:30 +0300

    Условия отбора данных

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,39 +1,76 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import com.sun.xml.bind.v2.model.core.ID;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstName")
   private String firstName;
+
   @Expose
+  @Column(name = "lastname")
   private String lastname;
+
+
+  @Transient
   private String address;
+
+  @Transient
   private String group;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+
+  @Transient
   @Expose
   private String email;
+
+  @Transient
   @Expose
   private String email2;
+
+  @Transient
   @Expose
   private String email3;
 
 
+  @Transient
   private String allPhones;
+  @Transient
   private String allEmails;
-  private File photo;
+
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
 
 
@@ -92,7 +129,7 @@ public class ContactData {
     return this;
   }
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -121,7 +158,7 @@ public class ContactData {
     return allEmails;
   }
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
   public int getId() { return id; }
 
